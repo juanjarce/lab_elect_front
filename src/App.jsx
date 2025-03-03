aab550a0608807a9c17548e4ca241940e9e2280e
Joki <josedavidamayar@gmail.com>
2025-03-03 13:52:36 -0500

    refactor: refactor the code and change docs

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,25 @@
-import React from 'react';
-import { HashRouter, Routes, Route } from 'react-router-dom';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import AdminDashboard from './pages/AdminDashboard';
-import EstudianteDashboard from './pages/EstudianteDashboard';
-import ChangePassword from './pages/ChangePassword';
-import VerificarCodigo from './pages/VerificarCodigo';
-import ProductosPestaña from './pages/ProductosPestaña'; // Importa el componente
-import Carrito from './pages/Carrito'; // Componente para la pestaña "Carrito"
-import Prestamos from './pages/Prestamos'; // Componente para la pestaña "Prestamos"
-import Cuenta from './pages/Cuenta'; // Componente para la pestaña "Cuenta"
-import Productos from './pages/Productos'; // Componente para la pestaña "Cuenta"
-import Solicitados from './pages/Solicitados'; // Componente para la pestaña "Cuenta"
-import Prestados from './pages/Prestados'; // Componente para la pestaña "Cuenta"
-import Devueltos from './pages/Devueltos'; // Componente para la pestaña "Cuenta"
-import GestionLaboratorios from './pages/GestionLaboratorios';
-import GestionReservas from './pages/GestionReservas';
-import Laboratorios from './pages/Laboratorios';
-import MisReservas from './pages/MisReservas';
-
-import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // Importa el JS de Bootstrap para que los colapsables funcionen
-
+import { HashRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import AdminDashboard from "./pages/AdminDashboard";
+import EstudianteDashboard from "./pages/EstudianteDashboard";
+import ChangePassword from "./pages/ChangePassword";
+import VerificarCodigo from "./pages/VerificarCodigo";
+import ProductosPestaña from "./pages/ProductosPestaña";
+import Carrito from "./pages/Carrito";
+import Prestamos from "./pages/Prestamos";
+import Cuenta from "./pages/Cuenta";
+import Productos from "./pages/Productos";
+import Solicitados from "./pages/Solicitados";
+import Prestados from "./pages/Prestados";
+import Devueltos from "./pages/Devueltos";
+import GestionLaboratorios from "./pages/GestionLaboratorios";
+import GestionReservas from "./pages/GestionReservas";
+import Laboratorios from "./pages/Laboratorios";
+import MisReservas from "./pages/MisReservas";
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 const App = () => {
   return (
@@ -30,23 +27,28 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/registro" element={<Register />} />
-        {/* AdminDashboard y sus subrutas */}
+        {/* AdminDashboard and subroutes */}
         <Route path="/admin-dashboard/:id" element={<AdminDashboard />}>
           <Route path="productos" element={<Productos />} />
           <Route path="prestamos/solicitados" element={<Solicitados />} />
           <Route path="prestamos/prestados" element={<Prestados />} />
           <Route path="prestamos/devueltos" element={<Devueltos />} />
-          <Route path="laboratorios/configuracion" element={<GestionLaboratorios />} />
+          <Route
+            path="laboratorios/configuracion"
+            element={<GestionLaboratorios />}
+          />
           <Route path="laboratorios/reservas" element={<GestionReservas />} />
-          {/* Aquí puedes agregar más rutas según las secciones del Admin */}
         </Route>
-        <Route path="/estudiante-dashboard/:id" element={<EstudianteDashboard />}>
-          {/* Subrutas específicas del dashboard del estudiante */}
+        <Route
+          path="/estudiante-dashboard/:id"
+          element={<EstudianteDashboard />}
+        >
+          {/* Subroutes for the student */}
           <Route path="productos" element={<ProductosPestaña />} />
           <Route path="carrito" element={<Carrito />} />
           <Route path="prestamos" element={<Prestamos />} />
           <Route path="cuenta" element={<Cuenta />} />
-          <Route path="laboratorios" element={<Laboratorios />} /> 
+          <Route path="laboratorios" element={<Laboratorios />} />
           <Route path="reservas" element={<MisReservas />} />
         </Route>
         <Route path="/verificar-codigo/:id" element={<VerificarCodigo />} />
